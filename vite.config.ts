1b96a67d3ae6eb3a6650e6665a319fdaa1186c72
TOLUWALASE <68949631+temitopetoluwalase@users.noreply.github.com>
2025-09-10 01:34:43 +0100

    configure vite vonfig to test github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
-import { componentTagger } from 'lovable-tagger';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: '::',
-    port: 8080,
-  },
-  plugins: [react(), mode === 'development' && componentTagger()].filter(
-    Boolean
-  ),
-  base: process.env.NODE_ENV === 'production' ? '/spark-plot-react/' : '/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-}));
+export default defineConfig({
+  plugins: [react()],
+  base: '/spark-plot-react/', // 👈 required for GitHub Pages
+});
